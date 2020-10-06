e7c61647a3eece80c24692bf42f77792696987bd
vicvondoom <davide.orlando1969@gmail.com>
2020-10-06 12:50:45 +0200

    Aggiungere i file di progetto.

---
+++ BOLAspNetCoreHelper/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using BOLAspNetCoreHelper.Models;
+using System.Globalization;
+
+namespace BOLAspNetCoreHelper.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private BOLHelper _bol;
+
+        public HomeController(ILogger<HomeController> logger, BOLHelper bol)
+        {
+            _logger = logger;
+            _bol = bol;
+        }
+
+        public IActionResult Index()
+        {
+            int c = 0;
+            try
+            {
+                int g = 5 / c;
+            }
+            catch (Exception ex)
+            {
+                _bol.Send(ex);
+            }
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ BOLAspNetCoreHelper/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BOLAspNetCoreHelper.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
